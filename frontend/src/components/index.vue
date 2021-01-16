d5bc3b693c040cbb20c6a2c3e197eb6cf57a6319
Seiya Miyata <odradek38@gmail.com>
2021-01-16 18:59:07 +0900

    Run `vue init webpack frontend`

---
+++ frontend/src/components/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div id="header"/>
+    <div>
+      <h1>夕ご飯何食べる？（ゆうたべ）</h1>
+      <h2>
+        {{message}}
+      </h2>
+      <h2>{{products}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data: function () {
+    return {
+      products: [],
+      // productName: '',
+      // productMemo: '',
+      message: 'hello, world!'
+    }
+  },
+
+  computed: {
+    computedProducts () {
+      return this.products
+    }
+  },
+
+  created: function () {
+    this.fetchProducts()
+  },
+
+  methods: {
+    fetchProducts () {
+      this.axios.get('/products').then(
+        res => {
+          if (res.status !== 200) {
+            throw new Error('response error')
+          } else {
+            var resultProducts = res.data
+            this.products = resultProducts
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
