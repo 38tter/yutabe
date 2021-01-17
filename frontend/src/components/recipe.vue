999bdafb2ebae8e467bf8696897215bcb61e0a8a
Seiya Miyata <odradek38@gmail.com>
2021-01-17 14:45:51 +0900

    Add subpage recipe.vue

---
+++ frontend/src/components/recipe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{$route.params.name}}</h1>
+    <img :src="$route.params.img" width="100%">
+    <h2>ingredients</h2>
+    <div v-for="(section,idx) in $route.params.sections" :key="idx">
+      <div v-for="component in section.components" :key="component.id">
+        {{component.raw_text}}
+      </div>
+    </div>
+    <h2>instructions</h2>
+    <div v-for="instruction in $route.params.instructions" :key="instruction.id">
+      {{instruction.display_text}}
+    </div>
+    <router-link :to="{name:'index'}" class="button">back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'recipe',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
+import recipe from '@/components/recipe'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/recipe',
+      name: 'recipe',
+      component: recipe
     }
   ]
 })
