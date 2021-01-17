d0e28d3b116a65bc28e2950d4b39da8be3c63037
Seiya Miyata <odradek38@gmail.com>
2021-01-17 18:14:01 +0900

    Add models

--- models/ingredient.go
+++ models/ingredient.go
@@ -4,5 +4,11 @@ import "github.com/jinzhu/gorm"
 
 type Ingredient struct {
 	gorm.Model
+	RecipeID uint
+	Name     string `gorm:"varchar(50);not null"`
+}
+
+type IngredientJson struct {
+	gorm.Model
 	Name string `gorm:"varchar(50);not null"`
 }
--- models/recipe.go
+++ models/recipe.go
@@ -7,18 +7,20 @@ type Recipe struct {
 	Name        string `gorm:"varchar(100);not null"`
 	ImageUrl    string `gorm:"varchar(100);not null"`
 	Description string `gorm:"varchar(100);not null"`
+	TastyID     int    `gorm:"int;not null"`
 	Instruction *Instruction
+	ingredients []*Ingredient
 	//rate         float64 `gorm:"varchar(100);not null"`
-	//ingredients  []*Ingredient
 	//utritions    []*Nutrition
 }
 
 type RecipeJson struct {
-	Name        string           `json:"name"`
-	ImageUrl    string           `json:"image_url"`
-	Description string           `json:"description"`
-	Instruction *InstructionJson `json:"instruction"`
+	Name        string            `json:"name"`
+	ImageUrl    string            `json:"image_url"`
+	Description string            `json:"description"`
+	TastyID     int               `json:"tasty_id"`
+	Instruction *InstructionJson  `json:"instruction"`
+	Ingredients []*IngredientJson `json:"ingredients"`
 	//rate         float64 `gorm:"varchar(100);not null"`
-	//ingredients  []*Ingredient
 	//utritions    []*Nutrition
 }
