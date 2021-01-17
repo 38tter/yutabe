fc4e545e60b21930cfe581b1394cffeb5c84fefd
Seiya Miyata <odradek38@gmail.com>
2021-01-17 15:26:00 +0900

    Move struct Product to package models

---
+++ models/product.go
@@ -0,0 +1,8 @@
+package models
+
+type Product struct {
+	ID          int    `gorm:"primary_key;not null"`
+	ProductName string `gorm:"type:varchar(200);not null"`
+	Memo        string `gorm:"type:varchar(400)"`
+	Status      string `gorm:"type:char(2);not null"`
+}
