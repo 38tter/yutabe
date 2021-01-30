6ef287bb2118ea3345b263744463797ac7e03346
Seiya Miyata <odradek38@gmail.com>
2021-01-30 15:06:28 +0900

    Remove unnecessary model product.go

--- models/product.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Product struct {
-	ID          int    `gorm:"primary_key;not null"`
-	ProductName string `gorm:"type:varchar(200);not null"`
-	Memo        string `gorm:"type:varchar(400)"`
-	Status      string `gorm:"type:char(2);not null"`
-}
